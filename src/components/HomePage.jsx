28b5f14f2708a9ac4cec4c89257ed3391bed362b
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-08 15:04:20 +0200

    Feat: write basic template HomePage component

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+  return (
+    <>
+      <h1>Welcome to the Random Store</h1>
+      <h2>For all your random needs</h2>
+
+      <img
+        src="../assets/general-store-269539_1280.jpg"
+        alt="Picture of our store front"
+      />
+    </>
+  );
+}
