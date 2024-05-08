9d4cf1db5feeb42dd5ead4dfda6edb82673a9af9
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-08 16:35:04 +0200

    Fix: import image to fix broken link view

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,12 @@
+import StorePic from "../assets/general-store-269539_1280.jpg";
+
 export default function HomePage() {
   return (
     <>
       <h1>Welcome to the Random Store</h1>
       <h2>For all your random needs</h2>
 
-      <img
-        src="../assets/general-store-269539_1280.jpg"
-        alt="Picture of our store front"
-      />
+      <img src={StorePic} alt="Picture of our store front" width={600} />
     </>
   );
 }
