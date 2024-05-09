173df664069eaf704fca0b6737a610336528baac
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-09 18:58:12 +0200

    Feat: edit items in cart + calculate total price
    * User can now edit items individually in the ShoppingCart and calculate
    total price after editing

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -3,10 +3,7 @@ import { useOutletContext } from "react-router-dom";
 
 export default function ShoppingCart() {
   const [cartItem, addToCart] = useOutletContext();
-
-  let totalCurrent = Object.entries(cartItem).map(
-    ([id, value]) => value.price * Number(value.items)
-  );
+  const [totalCurrent, setTotalCurrent] = useState(null);
 
   if (Object.keys(cartItem).length === 0) {
     return <h1>Your cart is empty</h1>;
@@ -17,24 +14,84 @@ export default function ShoppingCart() {
         <div className="shoppingCart">
           {Object.entries(cartItem).map(([id, value]) => {
             return (
-              <div className="shoppingCartItem" key={id}>
-                <h4>{value.title}</h4>
-                <p>
-                  <span>Price </span>
-                  {value.price * Number(value.items)}
-                </p>
-                <p>
-                  <span>Amount </span>
-                  {value.items}
-                </p>
-              </div>
+              <>
+                <CartItem
+                  id={id}
+                  value={value}
+                  addToCart={addToCart}
+                  cartItem={cartItem}
+                ></CartItem>
+              </>
             );
           })}
           <p>
-            <span>Total price: </span>
-            {totalCurrent.reduce((a, b) => a + b)}
+            <span>Total price: </span> {totalCurrent}
+            <button
+              onClick={() =>
+                setTotalCurrent(
+                  Object.entries(cartItem)
+                    .map(([id, value]) => value.price * Number(value.items))
+                    .reduce((a, b) => a + b)
+                )
+              }
+            >
+              Calculate total price
+            </button>
           </p>
         </div>
       </>
     );
+}
+
+function CartItem({ id, value, addToCart, cartItem }) {
+  const [editState, setEditState] = useState(false);
+  const [amount, setAmount] = useState(0);
+
+  return (
+    <div className="shoppingCartItem" key={id}>
+      <h4>{value.title}</h4>
+
+      <p>
+        <span>Price </span>
+        {value.price * Number(value.items)}
+      </p>
+
+      {!editState ? (
+        <p>
+          <span>Amount </span>
+          {value.items}
+          <button
+            onClick={() => {
+              setAmount(value.items);
+              setEditState(true);
+            }}
+            key={id}
+          >
+            Edit
+          </button>
+        </p>
+      ) : (
+        <>
+          <span>Amount </span>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              addToCart({
+                ...cartItem,
+                [id]: { ...cartItem[id], items: amount },
+              });
+              setEditState(false);
+            }}
+            key={id}
+          >
+            Submit
+          </button>
+        </>
+      )}
+    </div>
+  );
 }
