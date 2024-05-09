16796996bbe9c1437ed6929ff8bb481388648d29
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-09 15:23:13 +0200

    Feat: display total amount of items in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,24 +3,28 @@ import { Link } from "react-router-dom";
 import ShoppingCart from "./ShoppingCart";
 
 export default function NavBar({ cartItem }) {
-  const [currentPage, setCurrentPage] = useState("homepage");
+  let totalAmount = Object.entries(cartItem).map(([id, value]) => {
+    return Number(value.items);
+  });
+
   return (
     <>
       <nav>
         <h3>Navigation</h3>
         <ul>
-          {currentPage === "shoppage" ? (
-            <>
-              <Link to={"homepage"} onClick={() => setCurrentPage("homepage")}>
-                Home page
+          <>
+            <li>
+              <Link to={"homepage"}>Home page</Link>
+            </li>
+            <li>
+              <Link to={"shoppage"}>Shop page</Link>
+            </li>
+            <li>
+              <Link to={"shopcart"}>
+                Shopping cart ({totalAmount.reduce((a, b) => a + b)})
               </Link>
-              <ShoppingCart cartItem={cartItem} />
-            </>
-          ) : (
-            <Link to={"shoppage"} onClick={() => setCurrentPage("shoppage")}>
-              Shop page
-            </Link>
-          )}
+            </li>
+          </>
         </ul>
       </nav>
     </>
