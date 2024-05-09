7a7a5fa490414dc290574cd2e3c276a42d29c4ac
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-09 15:22:17 +0200

    Feat: add route to ShoppingCart

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./styles/index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./components/HomePage.jsx";
 import ShopPage from "./components/ShopPage.jsx";
+import ShoppingCart from "./components/ShoppingCart.jsx";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,7 @@ const router = createBrowserRouter([
       { index: true, element: <HomePage /> },
       { path: "homepage", element: <HomePage /> },
       { path: "shoppage", element: <ShopPage /> },
+      { path: "shopcart", element: <ShoppingCart /> },
     ],
   },
 ]);
