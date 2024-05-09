6053dcfe9a9120b3e91aae59368c407eb19c3b36
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-09 07:21:33 +0200

    Feat: load ShoppingCart on ShopPage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import ShoppingCart from "./ShoppingCart";
 
 export default function NavBar() {
   const [currentPage, setCurrentPage] = useState("homepage");
@@ -9,9 +10,12 @@ export default function NavBar() {
         <h3>Navigation</h3>
         <ul>
           {currentPage === "shoppage" ? (
-            <Link to={"homepage"} onClick={() => setCurrentPage("homepage")}>
-              Home page
-            </Link>
+            <>
+              <Link to={"homepage"} onClick={() => setCurrentPage("homepage")}>
+                Home page
+              </Link>
+              <ShoppingCart />
+            </>
           ) : (
             <Link to={"shoppage"} onClick={() => setCurrentPage("shoppage")}>
               Shop page
