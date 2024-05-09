85f0218536668016728d847c165db2f30e49ef6d
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-09 15:22:37 +0200

    Feat: display loading screen before items load

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,34 +1,40 @@
 import { useState } from "react";
+import { useOutletContext } from "react-router-dom";
 
-export default function ShoppingCart({ cartItem }) {
+export default function ShoppingCart() {
+  const [cartItem, addToCart] = useOutletContext();
+
   let totalCurrent = Object.entries(cartItem).map(
     ([id, value]) => value.price * Number(value.items)
   );
 
-  return (
-    <>
-      <h3>Items in your shopping cart</h3>
-      <div className="shoppingCart">
-        {Object.entries(cartItem).map(([id, value]) => {
-          return (
-            <div className="shoppingCartItem" key={id}>
-              <h4>{value.title}</h4>
-              <p>
-                <span>Price </span>
-                {value.price * Number(value.items)}
-              </p>
-              <p>
-                <span>Amount </span>
-                {value.items}
-              </p>
-            </div>
-          );
-        })}
-        <p>
-          <span>Total price: </span>
-          {totalCurrent.reduce((a, b) => a + b)}
-        </p>
-      </div>
-    </>
-  );
+  if (Object.keys(cartItem).length === 0) {
+    return <h1>Your cart is empty</h1>;
+  } else
+    return (
+      <>
+        <h3>Items in your shopping cart</h3>
+        <div className="shoppingCart">
+          {Object.entries(cartItem).map(([id, value]) => {
+            return (
+              <div className="shoppingCartItem" key={id}>
+                <h4>{value.title}</h4>
+                <p>
+                  <span>Price </span>
+                  {value.price * Number(value.items)}
+                </p>
+                <p>
+                  <span>Amount </span>
+                  {value.items}
+                </p>
+              </div>
+            );
+          })}
+          <p>
+            <span>Total price: </span>
+            {totalCurrent.reduce((a, b) => a + b)}
+          </p>
+        </div>
+      </>
+    );
 }
