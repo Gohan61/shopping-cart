58a194b07a694930822075c1fb5622c56570fd57
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-13 18:26:02 +0200

    Test: check if screen clears after add to Cart

--- tests/StoreItem.test.jsx
+++ tests/StoreItem.test.jsx
@@ -1,18 +1,25 @@
 import { render } from "@testing-library/react";
-import { describe, expect } from "vitest";
+import { describe, expect, it } from "vitest";
 import StoreItem from "../src/components/StoreItem";
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { func } from "prop-types";
 
 describe("render StoreItem component", () => {
-  it("renders props passed to StoreItem", () => {
-    const props = {
-      title: "Jacket",
-      price: 23,
-      description: "Nice piece",
-      image: "/path",
-      id: 0,
-    };
+  const props = {
+    title: "Jacket",
+    price: 23,
+    description: "Nice piece",
+    image: "/path",
+    id: 0,
+  };
+  const cartItemTest = { 1: { items: 1 } };
 
+  function addToCart() {
+    return cartItemTest;
+  }
+
+  it("renders props passed to StoreItem", () => {
     render(<StoreItem props={props}></StoreItem>);
 
     expect(screen.getByRole("heading").textContent).toMatch("Jacket");
@@ -21,5 +28,19 @@ describe("render StoreItem component", () => {
     expect(
       screen.getByRole("spinbutton", { type: "number" })
     ).toBeInTheDocument();
+  });
+
+  it("clears screen after add to cart", async () => {
+    render(
+      <StoreItem props={props} cartItem={cartItemTest} addToCart={addToCart} />
+    );
+
+    const user = userEvent.setup();
+
+    const button = screen.getByRole("button", { name: "Add to cart" });
+
+    await user.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch("");
   });
 });
