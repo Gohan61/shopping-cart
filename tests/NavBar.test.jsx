758eb70ff3fdae3ef99a03d74f192ef10a4f2293
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-05-13 15:53:06 +0200

    Test: check if NavBar renders cartItem amount

--- tests/NavBar.test.jsx
+++ tests/NavBar.test.jsx
@@ -4,14 +4,17 @@ import { describe, expect } from "vitest";
 import NavBar from "../src/components/Navbar";
 import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
+import { exact } from "prop-types";
 
 describe("Navbar component", () => {
   it("renders navbar correct", () => {
-    const cartItemTest = { 1: { value: 1 } };
+    const cartItemTest = { 1: { items: 1 } };
     render(<NavBar cartItem={cartItemTest} />, { wrapper: BrowserRouter });
 
     expect(screen.getByText(/Home page/)).toBeInTheDocument();
     expect(screen.getByText(/Shop page/)).toBeInTheDocument();
-    expect(screen.getByText(/Shopping cart/)).toBeInTheDocument();
+    expect(
+      screen.getByText("Shopping cart (1)", { exact: false })
+    ).toBeInTheDocument();
   });
 });
