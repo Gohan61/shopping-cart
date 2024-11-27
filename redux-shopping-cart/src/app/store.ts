106c63f1ff6c9a33a2161281654503115da47af7
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-11-27 16:04:19 +0100

    Feat: set amount + calculate totalPrice state
    * Amount is saved to state by passing through reducer so that totalPrice
    can be calculated in shopping cart
    * Title is saved to state so it can shown in shopping cart
    * Price of item is requested through the api so that correct price can
    be acquired an total price calculated

--- redux-shopping-cart/src/app/store.ts
+++ redux-shopping-cart/src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "../features/products/productSlice";
+import cartReducer from "../features/shoppingCart/cartSlice";
 
 const store = configureStore({
   reducer: {
     product: productReducer,
+    cart: cartReducer,
   },
 });
 
---
+++ redux-shopping-cart/src/features/shoppingCart/cartSlice.ts
@@ -0,0 +1,47 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { cartStateApi, SingleProductType } from "../../types/storeTypes";
+
+const initialState: cartStateApi = {
+  error: "",
+};
+
+export const fetchProduct = createAsyncThunk<SingleProductType, { id: number }>(
+  "product/fetchProduct",
+  (arg: { id: number }) => {
+    return fetch(`https://fakestoreapi.com/products/${arg.id}`, {
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+      },
+    }).then((res) => res.json());
+  }
+);
+
+const amountSlice = createSlice({
+  name: "amount",
+  initialState,
+  reducers: {
+    addAmount: (
+      state,
+      action: { payload: { id: number; inputAmount: number } }
+    ) => {
+      state[action.payload.id] = { amount: action.payload.inputAmount };
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchProduct.fulfilled, (state, action) => {
+      const id = action.payload.id;
+      state[id].totalPrice = action.payload.price * state[id].amount;
+      state[id].title = action.payload.title;
+    });
+    builder.addCase(fetchProduct.rejected, (state, action) => {
+      if (action.error.message) {
+        state.error = action.error.message;
+      }
+    });
+  },
+});
+
+export default amountSlice.reducer;
+export const { addAmount } = amountSlice.actions;
