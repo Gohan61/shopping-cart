1e636f197218d14d6dc819b2d0fef59b29299ca2
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-11-29 17:28:31 +0100

    Refactor: render products divided in pages
    * Products are rendered in pages to keep a better overview + reduce load
    rendering less items

--- redux-shopping-cart/src/features/products/AllProducts.tsx
+++ redux-shopping-cart/src/features/products/AllProducts.tsx
@@ -1,25 +1,52 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import store from "../../app/store";
 import { fetchAllProducts } from "./productSlice";
 import SingleProduct from "../../components/SingleProduct";
-import { ProductStateApi } from "../../types/storeTypes";
+import { ProductStateApi, SingleProductType } from "../../types/storeTypes";
 import { AppDispatch } from "../../app/store";
+import { currentPage } from "../paginator/pageSlice";
+import Paginator from "../../components/Paginator";
 
 export default function AllProducts() {
   const allProducts = useSelector((state: ProductStateApi) => state.product);
+  const currentData = useSelector(
+    (state: { page: { currentData: SingleProductType[] } }) =>
+      state.page.currentData
+  );
   const dispatch = useDispatch<AppDispatch>();
+
   useEffect(() => {
-    dispatch(fetchAllProducts());
+    dispatch(fetchAllProducts()).then((res) => {
+      if (res.meta.requestStatus === "fulfilled") {
+        const state = store.getState();
+        dispatch(
+          currentPage({ currentPage: 1, products: state.product.products })
+        );
+      }
+      setLoading(false);
+    });
   }, []);
+  const [loading, setLoading] = useState(true);
 
   return (
-    <div>
-      <h1>All products in our store</h1>
-      {allProducts.loading ? <p>Loading</p> : ""}
-      {allProducts.error ? <p>{allProducts.error}</p> : ""}
-      {allProducts.products.map((item) => {
-        return <SingleProduct props={item} key={item.id + "product"} />;
-      })}
-    </div>
+    <>
+      <div>
+        <h1>All products in our store</h1>
+        {allProducts.loading ? <p>Loading</p> : ""}
+        {allProducts.error ? <p>{allProducts.error}</p> : ""}
+        {loading
+          ? "Loading"
+          : currentData.map((item) => {
+              return <SingleProduct props={item} key={item.id + "product"} />;
+            })}
+      </div>
+      <Paginator
+        props={{
+          dataLength: allProducts.products.length,
+          currentData: allProducts.products,
+        }}
+      ></Paginator>
+    </>
   );
 }
