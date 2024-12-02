2176dac5b88183a39b1a8964ff52c969cb950cf8
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-12-02 16:56:35 +0100

    Feat: render a home page with some intro text

---
+++ redux-shopping-cart/src/components/Homepage.tsx
@@ -0,0 +1,35 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Fake Store</h1>
+
+      <section>
+        <h2>Reliable</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum, quae
+          ab provident asperiores assumenda dignissimos odio, alias ex, eaque
+          ratione voluptate. Laborum, aliquid dolor doloribus delectus
+          reprehenderit quo ex inventore.
+        </p>
+      </section>
+      <section>
+        <h2>Fast</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem,
+          repellat. Dolores quam, odio amet eum ea voluptas neque nam
+          consequuntur, quo molestiae vero. Nulla dolorem quod labore ab rerum
+          nostrum.
+        </p>
+      </section>
+      <section>
+        <h2>Secure</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id
+          reiciendis assumenda explicabo quia, ea odio expedita perferendis
+          voluptatem itaque, dolor quisquam similique omnis veniam eos adipisci
+          eum eaque tenetur! Assumenda?
+        </p>
+      </section>
+    </div>
+  );
+}
