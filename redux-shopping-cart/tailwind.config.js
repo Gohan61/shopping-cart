db74168fce57a4a783085373d7a58365dbe3b94b
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-12-02 18:55:18 +0100

    Fix: typos

--- redux-shopping-cart/tailwind.config.js
+++ redux-shopping-cart/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{ts, tsx}"],
+  content: ["./src/**/*.tsx", "./index.html"],
   theme: {
     extend: {},
   },
