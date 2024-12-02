981fb007273cbcffebc7474ffb2b9c5228dc6e69
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-12-02 09:13:44 +0100

    Feat: render + dispatch (current) categories

--- redux-shopping-cart/src/components/Navbar.tsx
+++ redux-shopping-cart/src/components/Navbar.tsx
@@ -1,8 +1,24 @@
+import { useDispatch, useSelector } from "react-redux";
 import ShoppingCart from "./ShoppingCart";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import {
+  fetchCategories,
+  setCategory,
+} from "../features/categories/categorySlice";
+import { AppDispatch } from "../app/store";
 
 export default function Navbar() {
   const [displayCart, setDisplayCart] = useState(false);
+  const categories = useSelector(
+    (state: { category: { categories: string[] } }) => state.category.categories
+  );
+  const dispatch = useDispatch<AppDispatch>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    dispatch(fetchCategories());
+    setLoading(false);
+  }, []);
 
   return (
     <nav>
@@ -10,6 +26,18 @@ export default function Navbar() {
         Shopping Cart
       </button>
       {displayCart ? <ShoppingCart></ShoppingCart> : ""}
+      {loading
+        ? "Loading"
+        : categories.map((category) => (
+            <button
+              onClick={() => dispatch(setCategory({ category: category }))}
+            >
+              {category}
+            </button>
+          ))}
+      <button onClick={() => dispatch(setCategory({ category: "All" }))}>
+        All products
+      </button>
     </nav>
   );
 }
