6ec59a8b647ff3ccba4fc4a8710322d2e034977e
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-12-02 16:20:59 +0100

    Refactor: fetch only with asc/desc categories
    * To reduce fetch functions/reducers, category names are passed into
    either ascending or descending fetching of that category

--- redux-shopping-cart/src/features/products/productSlice.ts
+++ redux-shopping-cart/src/features/products/productSlice.ts
@@ -7,18 +7,86 @@ const initialState: ProductState = {
   error: "",
 };
 
-export const fetchAllProducts = createAsyncThunk<
+export const fetchSortedProducts = createAsyncThunk<
   ApiProductType,
-  void,
-  { rejectValue: string }
->("product/fetchAllProducts", () => {
-  return fetch("https://fakestoreapi.com/products", {
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-      accept: "application/json",
-    },
-  }).then((res) => res.json());
+  { category: string }
+>("product/fetchSortedProducts", (arg: { category: string }) => {
+  if (arg.category === "All") {
+    return fetch("https://fakestoreapi.com/products?sort=desc", {
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+      },
+    }).then((res) => res.json());
+  } else if (arg.category === "electronics") {
+    return fetch(
+      "https://fakestoreapi.com/products/category/electronics?sort=desc",
+      {
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          accept: "application/json",
+        },
+      }
+    ).then((res) => res.json());
+  } else if (arg.category === "jewelery") {
+    return fetch(
+      "https://fakestoreapi.com/products/category/jewelery?sort=desc",
+      {
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          accept: "application/json",
+        },
+      }
+    ).then((res) => res.json());
+  } else if (arg.category === "mens's clothing") {
+    return fetch(
+      "https://fakestoreapi.com/products/category/men's clothing?sort=desc",
+      {
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          accept: "application/json",
+        },
+      }
+    ).then((res) => res.json());
+  } else {
+    return fetch(
+      "https://fakestoreapi.com/products/category/women's clothing?sort=desc",
+      {
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          accept: "application/json",
+        },
+      }
+    ).then((res) => res.json());
+  }
+});
+
+export const fetchSingleCategory = createAsyncThunk<
+  ApiProductType,
+  { category: string }
+>("category/fetchSingleCategory", (arg: { category: string }) => {
+  if (arg.category === "All") {
+    return fetch("https://fakestoreapi.com/products", {
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+      },
+    }).then((res) => res.json());
+  } else {
+    return fetch(`https://fakestoreapi.com/products/category/${arg.category}`, {
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+      },
+    }).then((res) => res.json());
+  }
 });
 
 const productSlice = createSlice({
@@ -26,14 +94,26 @@ const productSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchAllProducts.pending, (state) => {
+    builder.addCase(fetchSortedProducts.pending, (state) => {
       state.loading = true;
     });
-    builder.addCase(fetchAllProducts.fulfilled, (state, action) => {
+    builder.addCase(fetchSortedProducts.fulfilled, (state, action) => {
       // eslint-disable-next-line @typescript-eslint/no-unused-expressions
       (state.loading = false), (state.products = action.payload);
     });
-    builder.addCase(fetchAllProducts.rejected, (state, action) => {
+    builder.addCase(fetchSortedProducts.rejected, (state, action) => {
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      (state.loading = false), (state.error = action.error.message as string);
+    });
+    builder.addCase(fetchSingleCategory.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchSingleCategory.fulfilled, (state, action) => {
+      //   const currentState = store.getState().product;
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      (state.loading = false), (state.products = action.payload);
+    });
+    builder.addCase(fetchSingleCategory.rejected, (state, action) => {
       // eslint-disable-next-line @typescript-eslint/no-unused-expressions
       (state.loading = false), (state.error = action.error.message as string);
     });
