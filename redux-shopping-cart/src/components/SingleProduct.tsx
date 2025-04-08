c40ca96fa3cedb222e28dd6ca473c6d1113a53df
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2025-04-08 19:07:04 +0200

    Style: use icon add to cart + align on right

--- redux-shopping-cart/src/components/SingleProduct.tsx
+++ redux-shopping-cart/src/components/SingleProduct.tsx
@@ -3,16 +3,19 @@ import { SingleProductType } from "../types/storeTypes";
 import { useDispatch } from "react-redux";
 import { addAmount, fetchProduct } from "../features/shoppingCart/cartSlice";
 import { AppDispatch } from "../app/store";
+import addCartIcon from "../assets/add_shopping_cart.png";
 
 export default function SingleProduct({ props }: { props: SingleProductType }) {
   const [inputAmount, setInputAmount] = useState(0);
   const dispatch = useDispatch<AppDispatch>();
 
   return (
-    <div className="flex flex-col w-80 md:w-96 p-3 hover:shadow-lg rounded">
+    <div className="flex flex-col w-80 md:w-96 p-3 hover:shadow-lg rounded-sm border-b border-blue-950">
       <div className="md:h-24">
-        <h4 className="font-semibold">{props.title}</h4>
-        <p className="text-red-500 font-semibold">€ {props.price}</p>
+        <h4 className="font-semibold border-b border-gray-300">
+          {props.title}
+        </h4>
+        <p className="text-red-700 font-semibold">€ {props.price}</p>
       </div>
       <div className="my-2 p-1 border-2 rounded border-zinc-600 bg-slate-300">
         <img
@@ -22,13 +25,13 @@ export default function SingleProduct({ props }: { props: SingleProductType }) {
         />
         <p className="pl-2 text-sm">{props.description}</p>
       </div>
-      <div className="flex mt-auto">
-        <div>
-          <label htmlFor={props.id.toString()} className="text-sm">
+      <div className="flex mt-auto justify-end gap-2 items-end">
+        <div className="flex flex-col ">
+          <label htmlFor={props.id.toString()} className="text-sm font-bold">
             Amount
           </label>
           <input
-            className="rounded border-2"
+            className="rounded border-2 w-[70px]"
             type="number"
             id={props.id.toString()}
             value={inputAmount}
@@ -37,14 +40,19 @@ export default function SingleProduct({ props }: { props: SingleProductType }) {
           />
         </div>
         <button
-          className="bg-red-800 text-white rounded p-1 font-bold text-xs md:text-base hover:bg-red-950"
+          className="bg-red-800 text-white rounded px-2 h-[30px] font-bold text-xs md:text-base hover:bg-red-950"
           onClick={async () => {
             dispatch(addAmount({ inputAmount, id: props.id }));
             setInputAmount(0);
             dispatch(fetchProduct({ id: props.id }));
           }}
         >
-          Add to cart
+          <img
+            src={addCartIcon}
+            alt="Add to cart"
+            width="20px"
+            className="w-[20px]"
+          />
         </button>
       </div>
     </div>
